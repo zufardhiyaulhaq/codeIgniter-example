7d73546fe1e45d5b1efa0b520893626cf4d47821
Zufar <zufardhiyaulhaq@gmail.com>
2019-11-21 19:26:10 +0700

    as

---
+++ app-deployment.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+sed -i "s~#{image}~$ARTIFACT_IMAGE~g" codeigniter-example-deployment.json
+
+if [ -z $KUBE_TOKEN ]; then
+  echo "FATAL: Environment Variable KUBE_TOKEN must be specified."
+  exit ${2:-1}
+fi
+
+if [ -z $NAMESPACE ]; then
+  echo "FATAL: Environment Variable NAMESPACE must be specified."
+  exit ${2:-1}
+fi
+
+echo
+echo "Namespace $NAMESPACE"
+
+status_code=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta2/namespaces/$NAMESPACE/deployments/codeigniter-example-deployment" \
+    -X GET -o /dev/null -w "%{http_code}")
+
+if [ $status_code == 200 ]; then
+  echo
+  echo "Updating deployment"
+  curl --fail -H 'Content-Type: application/strategic-merge-patch+json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta2/namespaces/$NAMESPACE/deployments/codeigniter-example-deployment" \
+    -X PATCH -d @codeigniter-example-deployment.json
+else
+ echo
+ echo "Creating deployment"
+ curl --fail -H 'Content-Type: application/json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/apps/v1beta2/namespaces/$NAMESPACE/deployments" \
+    -X POST -d @codeigniter-example-deployment.json
+fi
+
+status_code=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/$NAMESPACE/services/codeigniter-example-service" \
+    -X GET -o /dev/null -w "%{http_code}")
+
+if [ $status_code == 404 ]; then
+ echo
+ echo "Creating service"
+ curl --fail -H 'Content-Type: application/json' -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
+    "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/$NAMESPACE/services" \
+    -X POST -d @codeigniter-example-service.json
+fi
