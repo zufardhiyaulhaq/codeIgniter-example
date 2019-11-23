a0abf884fb3632b99c7ced5f131bdb9b352ceb56
Zufar <zufardhiyaulhaq@gmail.com>
2019-11-23 09:41:19 +0700

    change url format?

--- app-build.sh
+++ app-build.sh
@@ -13,6 +13,5 @@ fi
 echo
 echo "Namespace $NAMESPACE"
 
-echo "curl --fail -H "Content-Type: application/json" -H "Authorization: Bearer $KUBE_TOKEN" 'https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/build.openshift.io/v1/namespaces/$NAMESPACE/buildconfigs/codeigniter-example-build/instantiate' -X POST"
 
-curl --fail -H "Content-Type: application/json" -H "Authorization: Bearer $KUBE_TOKEN" 'https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/build.openshift.io/v1/namespaces/$NAMESPACE/buildconfigs/codeigniter-example-build/instantiate' -X POST
+curl --fail -H 'Content-Type: application/json' -H "Authorization: Bearer $KUBE_TOKEN" "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/build.openshift.io/v1/namespaces/$NAMESPACE/buildconfigs/codeigniter-example-build/instantiate" -X POST
