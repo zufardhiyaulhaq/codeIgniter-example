3dcb58b72b8a6a6596ced4fa490200a14cffbf02
Zufar <zufardhiyaulhaq@gmail.com>
2019-11-23 08:56:49 +0700

    fix typo

--- app-build.sh
+++ app-build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-curl -k -v -XPOST  -H "Accept: application/json, */*" -H "Content-Type: application/json" -H "Authorization: Bearer $KUBE_TOKEN" 'https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/build.openshift.io/v1/namespaces/$NAMESPACE/buildconfigs/codeigniter-example-build/instantiate'
+curl --fail -XPOST -H "Content-Type: application/json" -H "Authorization: Bearer $KUBE_TOKEN" 'https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/build.openshift.io/v1/namespaces/$NAMESPACE/buildconfigs/codeigniter-example-build/instantiate'
