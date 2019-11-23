9306738e5eca69d1a20d42ed6996c5975fbea749
Zufar <zufardhiyaulhaq@gmail.com>
2019-11-23 10:18:15 +0700

    add verbose

--- app-build.sh
+++ app-build.sh
@@ -20,5 +20,5 @@ echo "Namespace $NAMESPACE"
 
 echo "curl --fail --cacert ${CACERT} -XPOST -H 'Accept: application/json, */*' -H 'Content-Type: application/json' -sSk -H 'Authorization: Bearer ${TOKEN}' 'https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/build.openshift.io/v1/namespaces/$NAMESPACE/buildconfigs/codeigniter-example-build/instantiate' "
 
-curl --fail --cacert ${CACERT} -XPOST -H 'Accept: application/json, */*' -H "Content-Type: application/json" -sSk -H "Authorization: Bearer ${TOKEN}" "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/build.openshift.io/v1/namespaces/$NAMESPACE/buildconfigs/codeigniter-example-build/instantiate" 
+curl --fail --cacert ${CACERT} -XPOST -H 'Accept: application/json, */*' -H "Content-Type: application/json" -sSk -H "Authorization: Bearer ${TOKEN}" "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/apis/build.openshift.io/v1/namespaces/$NAMESPACE/buildconfigs/codeigniter-example-build/instantiate"  -vvvv
 
